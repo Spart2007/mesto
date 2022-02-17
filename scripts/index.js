cddde25f460d0cba00d8a1b1b9ea56a35ffb2045
Antony <revenant2007@yandex.ru>
2022-02-18 00:32:09 +0300

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,6 @@ const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
 const profileEditButtonElement = document.querySelector(".profile__edit");
 
-const togglePopupVisibilyty = () => {
-    popupElement.classList.toggle("popup_open");
-}
 const openPopup = () => {
     popupElement.classList.add("popup_open");
 
@@ -26,13 +23,14 @@ const closePopupAfterClickOverlay = (event) => {
         closePopup();
     }
 }
+
 popupCloseButtonElement.addEventListener('click', closePopupAfterClickOverlay);
 
 // Ниже код для обработки события отправка формы
 const formElement = document.querySelector(".form");
 const formInput = formElement.querySelector(".form__input");
-const nameInput = formElement.querySelector(".form__input_name");
-const jobInput = formElement.querySelector(".form__input_profession");
+const nameInput = formElement.querySelector(".form_type_name");
+const jobInput = formElement.querySelector(".form_type_profession");
 const formSaveButton = formElement.querySelector(".form__save-button");
 
 const profileTitle = document.querySelector(".profile__title");
@@ -43,14 +41,10 @@ const formSubmitHandler = (evt) => {
 
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-
-    nameInput.textContent = profileTitle.value;
-    jobInput.textContent = profileSubtitle.value;
-
+    
     closePopup();
 
 };
 
 formInput.addEventListener('submit', formSubmitHandler);
-
 formSaveButton.addEventListener('click', formSubmitHandler);
