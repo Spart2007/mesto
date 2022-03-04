fdcd44a49fdea96f7c2cf60276d2185af4202817
Antony <revenant2007@yandex.ru>
2022-03-04 21:43:09 +0300

    add functional JS

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,7 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
 // переменные и константы
 // popup
 const popupProfile = document.querySelector(".popup_type_profile");
@@ -37,14 +38,87 @@ const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 // форма
 const formElement = document.querySelector(".form");
+//const formCard = document.queryselector(".form_type_card");
 const nameInput = formElement.querySelector(".form__input_type_name");
 const jobInput = formElement.querySelector(".form__input_type_profession");
 const titleInput = formElement.querySelector(".form__input_type_title")
-const linkInput = formElement.querySelector(".form__input_type_link")
+const linkInput = formElement.querySelector(".form__input_type_src")
+const cardContainer = document.querySelector(".elements__list")
+const formSaveButton = document.querySelector(".form__save-button")
+
 //like-button
 const likeMark = document.querySelectorAll(".element__like-button");
-//card
-const templateCard = document.querySelectorAll("#template-card").content;
+
+
+// init
+/*
+пробегаеися по initialCards
+и для каждого объекта создаем новый li
+в cardContainer
+*/
+// initialCards.slice(-6).forEach((item) => {
+//   const liEl = document.createElement('li')
+//   liEl.classList.add('element')
+//   liEl.innerHTML = `
+//      <button type="button" class="element__delite-button "></button>
+//      <img src="${item.link}" alt="${item.name}" class="element__mask-group">
+//      <div class="element__row">
+//        <h2 class="element__text">${item.name}</h2>
+//        <button class="element__like-button" type="button"></button>
+//     </div>
+//   `
+//   cardContainer.appendChild(liEl)
+// })
+
+
+
+function addElem (item) {
+  const templateElements = document.getElementById("template");
+  const clone = templateElements.content.cloneNode(true);  
+  clone.querySelector('img').setAttribute('src', item.link)
+  clone.querySelector('img').setAttribute('alt', item.name)
+  clone.querySelector('.element__text').innerText = item.name
+  clone.querySelector('.element__delete-button').addEventListener('click', () => {
+    const index = initialCards.findIndex(el => el.link == item.link)
+    initialCards.splice(index, 1)
+    render()
+    console.log(initialCards)
+  })
+  cardContainer.appendChild(clone)
+}
+
+
+function render () {
+  cardContainer.innerHTML = ''
+  initialCards.forEach((item) => {
+    addElem(item)
+  })  
+}
+
+
+formSaveButton.addEventListener('click', () => {
+  const newObj = {
+    name: '',
+    link: ''
+  }
+
+  // data.name = titleInput.value;
+  // data.link = linkInput.value;
+
+
+  initialCards.push(newObj)
+  render()
+})
+
+
+render()
+
+
+// for (el in initialCards) {
+//   cardContainer.createElement(el)
+// }
+
+// end init
 
 //код для открытия и закрытия popup
 const openPopup = (popupName) => {
@@ -78,14 +152,45 @@ const formSubmitHandler = (evt) => {
   evt.preventDefault(); //отменяет стандартную отправку формы 
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
+
   closePopup();
 };
 formElement.addEventListener('submit', formSubmitHandler);
 
+
 //код добавления карточки
+//template
+
+  const createCard = (item) => {
+    const card = newCard(item, templateElements);
+    return card.generateCard();
+  };
+
+const cardSubmitHandler = (evt) => {
+  evt.preventDefault(); //отменяет стандартную отправку формы 
+
+  const data = {};
+  data.name = titleInput.value;
+  data.link = linkInput.value;
+
+  const newCard = createCard(data);
+
+  closePopup();
+};
+
+// createCard.addEventListener('submit', cardSubmitHandler);
+
+/*initialCards.forEach((item) => {
+	const cardElement = createCard(item);
+	cardContainer.append(cardElement);
+});*/
 
 
-//код активации лайка на карточке
+
+
+
+
+//активация лайка на карточке
 for (let i = 0; i < likeMark.length; i++) {
   likeMark[i].addEventListener('click', () => {
    likeMark[i].classList.toggle('element__like-button_activated')});
